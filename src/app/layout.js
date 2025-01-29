0d403932213175ac915309aa98be5d9fa39ce48c
Adro1t <ramankhadgi0@gmail.com>
2025-01-29 12:28:19 +0545

    test1

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,38 +6,34 @@ export const metadata = {
   description: "Next.js application with Google Tag Manager integration",
 };
 
-// GTM container ID - replace with your actual ID
-const GTM_ID = "GTM-MVC4G9ZN"; // Replace with your valid GTM ID
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {/* GTM Script - placed at end of body */}
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
+      <head>
+        {/* <!-- Google Tag Manager --> */}
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `,
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MVC4G9ZN');
+`,
           }}
         />
+        {/* <!-- End Google Tag Manager --> */}
+      </head>
+      <body>
         {/* GTM noscript - placed at top of body */}
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MVC4G9ZN"
+            height={0}
+            width={0}
             style={{ display: "none", visibility: "hidden" }}
-          />
+          ></iframe>
         </noscript>
-
         {/* Main content */}
         {children}
       </body>
