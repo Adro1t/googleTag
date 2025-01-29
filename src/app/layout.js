50aafb7f9d779f7bf57176d1bf56503de0a7c4d4
Adro1t <ramankhadgi0@gmail.com>
2025-01-29 12:06:56 +0545

    added script instead of third party

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 export const metadata = {
   title: "Next.js",
@@ -9,7 +9,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <GoogleTagManager gtmId="GTM-MVC4G9ZN" />
+        {/* <!-- Google Tag Manager --> */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MVC4G9ZN');
+            `,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body>
         {children}
